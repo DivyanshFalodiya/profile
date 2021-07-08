14e2288209686fc44ed6e209ac77fa1b163f8fd7
DivyanshFalodiya <divyanshfalodiyaab@gmail.com>
2021-07-08 12:55:06 +0530

    :building_construction: set up the mvc arch base

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 // Require modules
 const express = require('express');
 const homeRoutes = require('./routes/homeRoutes');
+const projectRoutes = require('./routes/projectRoutes');
+const aboutRoutes = require('./routes/aboutRoutes');
+const loginRoutes = require('./routes/loginRoutes');
 
 // App and middlewares
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/public`));
+
+// Routes
 app.use('/', homeRoutes);
+app.use('/projects', projectRoutes);
+app.use('/about', aboutRoutes);
+app.use('/admin-login', loginRoutes);
 
 // Listening on port
 const PORT = process.env.PORT || 3000;

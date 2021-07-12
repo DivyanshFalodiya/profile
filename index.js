7dc12684c45a9912a1c36fd2071a5336490637e4
DivyanshFalodiya <divyanshfalodiyaab@gmail.com>
2021-07-12 14:57:08 +0530

    :adhesive_bandage: fixed the session bug

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 // Require modules
 const express = require('express');
+const session = require('express-session');
 const homeRoutes = require('./routes/homeRoutes');
 const projectRoutes = require('./routes/projectRoutes');
 const loginRoutes = require('./routes/loginRoutes');
 
 // App and middlewares
 const app = express();
+
+// Session
+app.set('trust proxy', 1);
+app.use(
+    session({
+        secret: process.env.SESSION_KEY || 'somerandomkey',
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            sameSite: 'strict',
+            secure: true,
+        },
+    })
+);
+
+// EJS and frontend
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/public`));
 app.use(express.json());

92ad1e163e8985d736829d3aa2d462ebba2b8cad
DivyanshFalodiya <divyanshfalodiyaab@gmail.com>
2021-07-28 23:41:55 +0530

    :alien: changed project api for more details

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -4,10 +4,11 @@ function fetchProjects() {
     const data = [];
     for (let i = 0; i < 5; i++) {
         const title = faker.name.jobTitle();
-        const image = 'https://source.unsplash.com/random';
         const about = faker.lorem.paragraph(3);
+        const tech = ['python', 'java', 'cplusplus'];
+        const image = 'https://source.unsplash.com/random';
         const link = 'https://github.com/DivyanshFalodiya';
-        data.push({ title, image, about, link });
+        data.push({ title, image, about, link, tech });
     }
     return data;
 }

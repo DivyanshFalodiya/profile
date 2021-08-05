2718490d4bd218aa17d933c8d293f7e3706023ca
DivyanshFalodiya <divyanshfalodiyaab@gmail.com>
2021-08-05 14:42:20 +0530

    :alien: added logout functionality

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,23 @@
 const router = require('express').Router();
 const authController = require('../controllers/authController');
 
+// Get Login route
 router.get('/', (req, res) => {
     res.render('login', { error: '', success: '' });
 });
 
+// Get Logout route
+router.get('/logout', (req, res) => {
+    res.cookie('jwt', '');
+    res.redirect('/');
+});
+
+// Callback for login auth
 router.get('/callback', authController.isAuthenticated, (req, res) => {
     res.redirect('/');
 });
 
+// Post request to login
 router.post('/', authController.login);
 
 module.exports = router;

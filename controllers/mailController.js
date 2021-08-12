9a335b5211511de003872977452e23d6a141eaf6
DivyanshFalodiya <divyanshfalodiyaab@gmail.com>
2021-08-12 13:14:59 +0530

    :bug: still fixing nodemailer

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -2,18 +2,22 @@ require('dotenv').config();
 const nodeMailer = require('nodemailer');
 
 const transporter = nodeMailer.createTransport({
-    // host: 'smtp.gamil.com',
-    // port: 465,
-    service: 'Gmail',
+    host: 'smtp.gamil.com',
+    port: 465,
+    service: 'gmail',
     secure: true,
+    // authMethod: 'XOAUTH2',
     // requireTLS: true,
     auth: {
         type: 'OAuth2',
         user: process.env.HOST_ID,
-        scope: 'https://mail.google.com/',
+        // scope: 'https://mail.google.com/',
         clientId: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
         refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: process.env.ACCESS_TOKEN,
+        // serviceClient: process.env.GOOGLE_SERVICE_ID,
+        // privateKey: process.env.GOOGLE_PRIVATE_KEY,
     },
 });
 

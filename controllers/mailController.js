80397bd52e96f611139e633783d33889c817940b
DivyanshFalodiya <divyanshfalodiyaab@gmail.com>
2021-08-12 15:29:55 +0530

    :bug: fixed nodemailer finally lolllllll

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -6,18 +6,12 @@ const transporter = nodeMailer.createTransport({
     port: 465,
     service: 'gmail',
     secure: true,
-    // authMethod: 'XOAUTH2',
-    // requireTLS: true,
     auth: {
         type: 'OAuth2',
         user: process.env.HOST_ID,
-        // scope: 'https://mail.google.com/',
         clientId: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
         refreshToken: process.env.REFRESH_TOKEN,
-        // accessToken: process.env.ACCESS_TOKEN,
-        // serviceClient: process.env.GOOGLE_SERVICE_ID,
-        // privateKey: process.env.GOOGLE_PRIVATE_KEY,
     },
 });
 

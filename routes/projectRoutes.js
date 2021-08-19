a64342aa0bfe7268198838b3856aa32b01f8c768
DivyanshFalodiya <divyanshfalodiyaab@gmail.com>
2021-08-19 14:34:44 +0530

    :alien: added route to particular project

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -7,23 +7,41 @@ const Project = require('../models/ProjectModel');
 router.get('/', async (req, res) => {
     const projects = await projectController.fetchProjects();
     if (projects != null) {
+        // const cookies = req.cookies;
+        // if (!cookies.jwt) {
+        //     res.render('projects', { projects: projects, admin: false });
+        //     return;
+        // }
+        // const email = authController.verifyToken(cookies.jwt);
+        // if (email === process.env.ADMIN_MAIL) {
+        //     res.render('projects', { projects: projects, admin: true });
+        //     return;
+        // }
+        res.render('projects', { projects: projects });
+    }
+    res.status(501);
+});
+
+router.get('/:id', async (req, res) => {
+    const project = await projectController.fetchProject(req.params.id);
+    if (project != null) {
         const cookies = req.cookies;
         if (!cookies.jwt) {
-            res.render('projects', { projects: projects, admin: false });
+            res.render('project', { project: project, admin: false });
             return;
         }
         const email = authController.verifyToken(cookies.jwt);
         if (email === process.env.ADMIN_MAIL) {
-            res.render('projects', { projects: projects, admin: true });
+            res.render('project', { project: project, admin: true });
             return;
         }
-        res.render('projects', { projects: projects, admin: false });
+        res.render('project', { project: project, admin: false });
     }
     res.status(501);
 });
 
 // Admin only route for updating project
-router.get('/edit/:id', authController.isAuthenticated, async (req, res) => {
+router.get('/:id/edit', authController.isAuthenticated, async (req, res) => {
     const project = await projectController.fetchProject(req.params.id);
     if (project) {
         res.render('edit', { project, error: '', success: '' });

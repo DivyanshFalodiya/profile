34af74f01eb02487cd23b4057631f66773cc9fb1
DivyanshFalodiya <divyanshfalodiyaab@gmail.com>
2021-08-25 00:24:08 +0530

    :children_crossing: subtle changes

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -14,31 +14,30 @@ class Index {
         this.animateRoles();
     }
 
-    changeIndex() {
-        this.index = this.index + 1 == this.roles.length ? 0 : this.index + 1;
-    }
-
     animateRoles() {
         let cur = this.roles[this.index];
         this.displayRole(cur);
         setTimeout(() => {
             this.removeRole(cur);
-        }, this.interval * cur.length + this.timeGap);
+        }, this.interval * (cur.length + 1) + 3 * this.timeGap);
         setTimeout(() => {
+            this.index =
+                this.index + 1 == this.roles.length ? 0 : this.index + 1;
             this.animateRoles();
-        }, this.interval * cur.length * 2 + 3 * this.timeGap);
+        }, this.interval * (cur.length + 1) * 2 + 3 * this.timeGap);
     }
 
     displayRole(cur) {
         let charIndex = 0;
         let id = setInterval(() => {
             this.rolesContainer.textContent =
-                this.rolesContainer.textContent + cur[charIndex];
+                this.rolesContainer.textContent +
+                (cur[charIndex] ? cur[charIndex] : '');
             charIndex++;
         }, this.interval);
         setTimeout(() => {
             clearInterval(id);
-        }, this.interval * cur.length);
+        }, this.interval * (cur.length + 1));
     }
     removeRole(cur) {
         let charIndex = cur.length - 1;
@@ -49,8 +48,7 @@ class Index {
         }, this.interval);
         setTimeout(() => {
             clearInterval(id);
-            this.changeIndex();
-        }, this.interval * cur.length + this.timeGap);
+        }, this.interval * (cur.length + 1) + this.timeGap);
     }
 
     stopRender() {}

d03986571d6413d31594d8d0ab5e9cef7d8a3702
DivyanshFalodiya <divyanshfalodiyaab@gmail.com>
2021-09-03 16:05:43 +0530

    :lipstick: subtle upgrades

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,40 +1,7 @@
 const router = require('express').Router();
+const skills = require('../stuff/skills');
 
 router.get('/', (req, res) => {
-    skills = [
-        'devicon-cplusplus-plain',
-        'devicon-java-plain',
-        'devicon-c-plain',
-        'devicon-python-plain',
-        'devicon-kotlin-plain',
-        'devicon-javascript-plain',
-        'devicon-jquery-plain',
-        'devicon-npm-original-wordmark',
-        'devicon-numpy-original',
-        'devicon-html5-plain',
-        'devicon-css3-plain',
-        'devicon-bootstrap-plain',
-        'devicon-materialui-plain',
-        'devicon-mongodb-plain',
-        'devicon-mysql-plain',
-        'devicon-threejs-original',
-        'devicon-nodejs-plain',
-        'devicon-react-plain',
-        'devicon-nextjs-plain',
-        'devicon-firebase-plain',
-        'devicon-socketio-original',
-        'devicon-android-plain',
-        'devicon-gradle-plain',
-        'devicon-heroku-plain',
-        'devicon-illustrator-plain',
-        'devicon-gimp-plain',
-        'devicon-visualstudio-plain',
-        'devicon-vscode-plain',
-        'devicon-xd-plain',
-        'devicon-trello-plain',
-        'devicon-git-plain',
-        'devicon-github-plain',
-    ];
     res.render('index', { skills: skills });
 });
 
